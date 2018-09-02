51f45ca384a587795eb461e063b8750a8be56550
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-09-01 22:49:53 -0400

    builders tests setup

---
+++ tests/builders/test_approximators_builder.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestApproximatorBuilder(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_build_DeepConvolutionalValue(self):
+        pass
+
+    def test_build_DeepDenseValue(self):
+        pass
