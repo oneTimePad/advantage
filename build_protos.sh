8d72a20f1a4963331f3bab3315136500820940a7
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-09-05 21:47:45 -0400

    scripts for clearing and building pb2.py

---
+++ build_protos.sh
@@ -0,0 +1,5 @@
+protoc  protos/approximators/* --python_out=.
+protoc  protos/agents/* --python_out=.
+protoc  protos/models/* --python_out=.
+protoc  protos/configuration.proto --python_out=.
+protoc  protos/environments.proto --python_out=.
---
+++ clear_pb2.sh
@@ -0,0 +1,9 @@
+rm ./protos/agents/*_pb2.py
+rm ./protos/approxmators/*_pb2.py
+rm ./protos/models/*_pb2.py
+rm ./protos/*_pb2.py
+
+rm -r ./protos/agents/__pycache__
+rm -r ./protos/approximators/__pycache__
+rm -r ./protos/models/__pycache
+rm -r ./protos/__pycache__
