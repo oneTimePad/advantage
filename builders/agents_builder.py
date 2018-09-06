0beff6d09fdb461e766fb3598dc1e68b28c9721e
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-09-05 20:50:02 -0400

    builders: agents builder init

---
+++ builders/agents_builder.py
@@ -0,0 +1,13 @@
+
+
+
+def build(agents_config, environment):
+    """ Builds an Agent based on configuration
+            Args:
+                agents_config: configuration from protobuf for agent
+                environment: OpenAi Gym Gym object
+
+            Returns:
+                an Agent object
+    """
+    pass
