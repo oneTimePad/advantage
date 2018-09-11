43713c23cab35974e0a81f13e7d87f0adfdcb0ec
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-09-10 21:23:06 -0400

    scripts: added tests for util

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -10,3 +10,7 @@ python3 tests/approximators/test_deep_dense.py
 #builders
 python3 tests/builders/test_agents_builder.py
 python3 tests/builders/test_approximators_builder.py
+
+#utils
+python3 tests/utils/test_buffers.py
+python3 tests/utils/test_values.py
