73dac20efeb83c0db83fdf4ca6199a9897b2bb9f
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-11-25 17:57:24 -0500

    dqn-agent: removed unused imports

--- agents/deep_q_agent.py
+++ agents/deep_q_agent.py
@@ -1,5 +1,3 @@
-from functools import partial
-import numpy as np
 import tensorflow as tf
 from advantage.agents.base.base_agents import OffPolicyValueAgent
 from advantage.agents.base.approximate_agents import ApproximateAgent
