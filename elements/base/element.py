10b18de0f62acbf6d51b78cc914312212eb5c9fd
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-11-25 15:02:47 -0500

    elements: dtype issue

--- elements/base/element.py
+++ elements/base/element.py
@@ -11,7 +11,7 @@ Reinforcement learning paradigm. The internal module used in the attr module. Th
 by theElement class. So the client only has to import one module.
 """
 
-class NumpyElementMixin(object):
+class NumpyElementMixin:
     """ Mixin for a Element that uses Numpy.ndarray
     attrs
     """
@@ -20,9 +20,11 @@ class NumpyElementMixin(object):
     def _validate(dtype):
         def valid(instance, attribute, value):
             if not isinstance(value, np.ndarray):
-                raise ValueError("%s must be of type np.ndarray" % attribute.name)
+                raise ValueError("%s must be of type np.ndarray"
+                                 " but was %s" % (attribute.name, type(value)))
             if not value.dtype == dtype:
-                raise ValueError("%s must have dtype %s" % (attribute.name, str(dtype)))
+                raise ValueError("%s must have dtype %s but had dtype=%s"
+                                 % (attribute.name, str(dtype), str(value.dtype)))
         return valid
 
     @classmethod
@@ -34,9 +36,9 @@ class NumpyElementMixin(object):
                        metadata={"np_attr" : True})
 
     @classmethod
-    def reduce(cls, element_list):
-        """ Fetches np_element's from each BufferType in the list
-        and splits into a BufferElement containg th
+    def stack(cls, element_list):
+        """ Combines common np_attrs in the elements in the list
+        into one np_attr (stacked np.ndarray)
 
             Args:
                 element_list: list of BufferElement's to reduce
@@ -59,153 +61,173 @@ class NumpyElementMixin(object):
             for name, name_list in np_attrs_dict.items():
                 name_list.append(getattr(element, name))
 
-        np_attrs_dict_stacked = {k: np.vstack(v) for k, v in np_attrs_dict.items()} # TODO: maybe wrap this exception (fail to stack)
+        np_attrs_dict_stacked = {k: np.vstack(v) for k, v in np_attrs_dict.items()}
 
         return cls.make_element_from_dict(np_attrs_dict_stacked)
 
 
 
-class NormalizingElementMixin(object):
-        """Mixin for a Element that allows for Normalization of some or all
-        of it's attributes. A lot RL algorithms utilizing Approximators require
-        Normalization of their inputs
+class NormalizingElementMixin:
+    """Mixin for a Element that allows for Normalization of some or all
+    of it's attributes. A lot RL algorithms utilizing Approximators require
+    Normalization of their inputs
+    """
+
+    @staticmethod
+    def running_variance(sums, sums_sqr, num):
+        """Computes running variance from running sum of data,
+           running sum of squares of data and data count
+
+            Args:
+                sums: running sum of data
+                sums_sqr: running sum of square of data
+                num: running count of data
+
+            Returns:
+                running variance
+        """
+        variance = (1. / num) * (sums_sqr - ((sums ** 2) / num))
+        return variance.astype(np.float32)
+
+
+    @classmethod
+    def make_stats(cls, normalize_attrs=(), **kwargs_to_make_element_zero):
+        """ Returns a dictionary of attrs to keep track of for
+        normalizing. Used internally by this mixin
+            Args:
+                normalize_attrs: tuple of str representing which attrs to normalize
+                kwargs_to_make_element_zero: args to pass to make_element_zero
+
+            Returns:
+                normalizing: dict containg attrs to keep track of
+
+            Raises:
+                ValueError: missing attrs, or bad attrs
         """
 
-        @staticmethod
-        def running_variance(sums, sums_sqr, num):
-            """Computes running variance from running sum of data, running sum of squares of data and data count
+        if not normalize_attrs:
+            raise ValueError("normalize_attrs expects some attrs to normalize")
+
+        if not hasattr(cls, "make_element_zero"):
+            raise ValueError("Mixin expects subclass to contain attribute make_element_zero, \
+                should subclass Element")
+
+
+
+        if not hasattr(cls, "__attrs_attrs__"):
+            raise ValueError("Object returned from make_element_zero must have"
+                             " attribute __attrs_attrs__, should subclass Element"
+                             " and decorated with BufferElement.element")
+
+        normalizing = {}
+
+        attrs_to_normalize = filter(lambda x: x.name in normalize_attrs, cls.__attrs_attrs__)
+
+        for attribute in attrs_to_normalize:
+            attr_name = attribute.name
+            normalizing[attr_name] = (getattr(cls.make_element_zero(**kwargs_to_make_element_zero),
+                                              attr_name),
+                                      getattr(cls.make_element_zero(**kwargs_to_make_element_zero),
+                                              attr_name))
+        return normalizing
+
+    @staticmethod
+    def update_normalize_stats(stats, element):
+        """ Updates a stats object with attrs from a new element
 
                 Args:
-                    sums: running sum of data
-                    sums_sqr: running sum of square of data
-                    num: running count of data
+                    stats: as returned from make_stats
+                    element: element to aggregate
 
-                Returns:
-                    running variance
-            """
+                mutates stats
+        """
 
-            return (1 / num) * (sums_sqr - ( (sums ** 2) / num) )
+        for attribute_name, stat_tup in stats.items():
+            sums, sums_sqr = stat_tup
+            value = getattr(element, attribute_name)
 
+            sums_sqr += value ** 2
+            sums += value
 
-        @classmethod
-        def make_stats(cls, normalize_attrs=(), **kwargs_to_make_element_zero):
-            """ Returns a dictionary of attrs to keep track of for
-            normalizing. Used internally by this mixin
+    @classmethod
+    def normalize_element(cls, stats, number_elements, element, eps=0.01):
+        """ Normalizes an element based on stats.
+                To take advantage of numpy broadcasting, look for
+                reduce method in mixins
+
                 Args:
-                    normalize_attrs: tuple of str representing which attrs to normalize
-                    kwargs_to_make_element_zero: args to pass to make_element_zero
+                    stats: as returned from make_stats
+                    number_elements: number of elements used to update stats
+                    element: element to normalize
+                    eps: divide-by-zero protection threshold
+        """
+        if not hasattr(cls, "make_element_from_dict"):
+            raise ValueError("Mixin expects subclass to contain attribute make_element_from_dict, \
+                should subclass Element")
 
-                Returns:
-                    normalizing: dict containg attrs to keep track of
+        new_attrs_dict = {attribute.name: getattr(element, attribute.name)
+                          for attribute in element.__attrs_attrs__}
 
-                Raises:
-                    ValueError: missing attrs, or bad attrs
-            """
+        for attr_name, stats_tup in stats.items():
+            sums, sums_sqr = stats_tup
 
-            if not normalize_attrs:
-                raise ValueError("normalize_attrs expects some attrs to normalize")
+            variance = cls.running_variance(sums, sums_sqr, number_elements)
 
-            if not hasattr(cls, "make_element_zero"):
-                raise ValueError("Mixin expects subclass to contain attribute make_element_zero, \
-                    should subclass Element")
+            mean = (1. / number_elements) * sums
 
+            value = getattr(element, attr_name)
 
+            normalized_attr = (value - mean) / np.sqrt(np.maximum(variance, eps))
 
-            if not hasattr(cls, "__attrs_attrs__"):
-                raise ValueError("Object returned from make_element_zero must have attribute __attrs_attrs__, \
-                    should subclass Element and decorated with BufferElement.element")
+            new_attrs_dict[attr_name] = normalized_attr
 
-            normalizing = {}
+        return cls.make_element_from_dict(new_attrs_dict)
 
-            attrs_to_normalize = filter(lambda x: x.name in normalize_attrs, cls.__attrs_attrs__)
-
-            for attribute in attrs_to_normalize:
-                attr_name = attribute.name
-                normalizing[attr_name] = (getattr(cls.make_element_zero(**kwargs_to_make_element_zero), attr_name),
-                                            getattr(cls.make_element_zero(**kwargs_to_make_element_zero), attr_name))
-
-            return normalizing
-
-        @staticmethod
-        def update_normalize_stats(stats, element):
-            """ Updates a stats object with attrs from a new element
-
-                    Args:
-                        stats: as returned from make_stats
-                        element: element to aggregate
-
-                    mutates stats
-            """
-
-            for attribute_name, stat_tup in stats.items():
-                sums, sums_sqr = stat_tup
-
-                value = getattr(element, attribute_name)
-                sums_sqr += value ** 2
-                sums += value
-
-        @classmethod
-        def normalize_element(cls, stats, number_elements, element, eps=0.01):
-            """ Normalizes an element based on stats.
-                    To take advantage of numpy broadcasting, look for
-                    reduce method in mixins
-
-                    Args:
-                        stats: as returned from make_stats
-                        number_elements: number of elements used to update stats
-                        element: element to normalize
-                        eps: divide-by-zero protection threshold
-            """
-            if not hasattr(cls, "make_element_from_dict"):
-                raise ValueError("Mixin expects subclass to contain attribute make_element_from_dict, \
-                    should subclass Element")
-
-            new_attrs_dict = {attribute.name: getattr(element, attribute.name) for attribute in element.__attrs_attrs__}
-
-            for attr_name, stats_tup in stats.items():
-                sums, sums_sqr = stats_tup
-
-                variance = cls.running_variance(sums, sums_sqr, number_elements)
-                mean = (1 / number_elements) * sums
-
-                value = getattr(element, attr_name)
-
-                normalized_attr = (value - mean) / np.sqrt(np.maximum(variance, eps))
-
-                new_attrs_dict[attr_name] = normalized_attr
-
-            return cls.make_element_from_dict(new_attrs_dict)
-
-
-
-
-class Element(object, metaclass=ABCMeta):
+class Element(metaclass=ABCMeta):
     """ Represents a type to be placed in a Replay buffer (however, they can be used
     for other purposes as well, and do not require a buffer)
     There purpose is to serve as keepers of steps taken by the agent
     and the associated changes in the environment.
     """
 
+    # decorate sublcass with `Element.element`
+    # to set this correctly
+    __attrs_attrs__ = []
+
     @staticmethod
-    def element(fn):
-        """ Wrap attr in Element """
-        return attr.s(fn, frozen=True)
+    def element(wrapped_cls):
+        """ Wrap attr in Element
+                Args:
+                    wrapped_cls: cls to decorate
+        """
+        return attr.s(wrapped_cls, frozen=True)
 
     def unzip_to_tuple(self):
-        """ Puts attributes to tuple """
+        """ Puts attributes to tuple
+
+        """
         return tuple((getattr(self, attribute.name) for attribute in self.__attrs_attrs__))
 
     def unzip_to_dict(self):
+        """ Puts attributes in dict
+        """
         return {attribute.name: getattr(self, attribute.name) for attribute in self.__attrs_attrs__}
 
 
     def deep_copy(self):
+        """ Performs deep copy
+        of element
+        """
         return self.make_element_from_dict(self.unzip_to_dict())
 
     @classmethod
     def make_element_from_dict(cls, dictionary):
+        """ Factory: consructs element from dict of
+        corresponding attrs
+        """
         return cls.make_element(**dictionary)
 
+    @classmethod
     @abstractmethod
     def make_element_zero(cls, **kwargs):
         """ Factory: Instantiates a 'zeroed' out Element
@@ -217,6 +239,7 @@ class Element(object, metaclass=ABCMeta):
         """
         raise NotImplementedError()
 
+    @classmethod
     @abstractmethod
     def make_element(cls, **kwargs):
         """ Factory: Instantiates a Element of subclass 'cls'
