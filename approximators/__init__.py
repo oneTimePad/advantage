1aacdf9e8e2d0b82efae9da42d67590cf01bc302
Dylan Herman <dah242@scarletmail.rutgers.edu>
2018-12-31 17:57:44 -0500

    removed old approximator modules

--- approximators/__init__.py
+++ approximators/__init__.py
@@ -1,2 +1,2 @@
-from advantage.approximators.deep_convolutional import DeepConvolutional
-from advantage.approximators.deep_dense import DeepDense
+from advantage.approximators.deep_convolutional import DeepConvolutional, ConvBlock
+from advantage.approximators.deep_dense import DeepDense, DenseBlock
