54e18edf0d369d8d54875479c1724c2c380025ad
Dylan Herman <dah242@scarletmail.rutgers.edu>
2019-01-13 21:14:18 -0500

    Add gin utils

--- utils/gin_utils.py
+++ utils/gin_utils.py
@@ -1,7 +1,11 @@
+from advantage.utils.decorator_utils import parameterized
 import gin
 
+""" Utilities for gin configuration
+"""
+
 def gin_bind(cls, parameter, value, name_scope=""):
-    """ shortcut for using gin.bind_parameter
+    """ Shortcut for using gin.bind_parameter
             Args:
                 cls: class running from
                 parameter: gin param name
@@ -12,7 +16,7 @@ def gin_bind(cls, parameter, value, name_scope=""):
     gin.bind_parameter(param_str, value)
 
 def gin_bind_init(cls, parameter, value, name_scope=""):
-    """ shortcut for using gin.bind_parameter for classes
+    """ Shortcut for using gin.bind_parameter for classes
             Args:
                 cls: class running from
                 parameter: gin param name
@@ -21,3 +25,16 @@ def gin_bind_init(cls, parameter, value, name_scope=""):
     """
     param_str = "{0}/{1}/{2}.{3}".format(name_scope, cls.name_scope, cls.__name__, parameter)
     gin.bind_parameter(param_str, value)
+
+@parameterized
+def gin_classmethod(func, *args, **kwargs):
+    """ Enables gin configuration for classmethods
+            Args:
+                func: classmethod to configure
+            Returns
+                decorated classmethod
+    """
+    gin_func = gin.configurable(func, *args, **kwargs)
+    clsmth = classmethod(gin_func)
+    clsmth.__name__ = func.__name__
+    return clsmth
